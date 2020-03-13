c7d34008f1b1fab7604d2bef0bcbb1e4e4a944ff
Donald <448641125@qq.com>
2020-03-13 23:57:20 +0800

    init test

---
+++ index.js
@@ -0,0 +1,32 @@
+// console.log("hello world");
+
+// console.log(process);
+
+var playerAction = process.argv[process.argv.length - 1];
+
+var random = Math.random() * 3;
+
+if (random < 1) {
+
+    var computerAction = 'rock';
+} else if (random > 2) {
+
+    var computerAction = 'scissor';
+} else {
+
+    var computerAction = 'paper';
+}
+
+if (computerAction == playerAction) {
+
+    console.log('平局');
+} else if (
+    (computerAction === 'rock' && playerAction === 'paper') ||
+    (computerAction === 'scissor' && playerAction === 'rock') ||
+    (computerAction === 'paper' && playerAction === 'scissor') 
+) {
+    console.log('你赢了');
+} else {
+
+    console.log('你输了');
+}
