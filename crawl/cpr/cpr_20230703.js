07e22e4649ebcc18b82d8d2cf030bbc407095d74
yyf <448641125@qq.com>
2023-07-05 15:13:46 +0800

    feat: update crawl

--- crawl/cpr/cpr_20230703.js
+++ crawl/cpr/cpr_20230703.js
@@ -5,23 +5,46 @@ const cheerio = require('cheerio');
 const SAVE_FOLDER_PATH = '/Users/yangyf/Downloads';
 const INDEX_NAME = "INDEX";
 const PAGE_INDEX = "http://www.customs.gov.cn/customs/302249/zfxxgk/2799825/302274/302277/4899681/index.html";
+const DOMAIN = "http://www.customs.gov.cn/";
+
 
 function runMain() {
     console.log("begin...");
 
     let yearPageList = getYearPageList(INDEX_NAME, PAGE_INDEX);
+    console.log(yearPageList);
+    // for (let i = 0; i < yearPageList.length; ++i) {
+    //     let yearPage = yearPageList[i];
+    //     const folderName = yearPage.year;
+    //     const folderPath = `${SAVE_FOLDER_PATH}/${folderName}`;
+    //     fs.mkdir(folderPath, { recursive: true }, (err) => {
+    //         if (err) {
+    //             console.error(`创建文件夹 ${folderPath} 时遇到错误：${err}`);
+    //         } else {
+    //             console.log(`文件夹 ${folderPath} 创建成功！`);
+    //         }
+    //     });
+    //
+    // }
 
     console.log("end...");
 }
 
 function getYearPageList(name, pageUrl) {
-    const HTML = catchPageHTML(name, pageUrl) || '';
-    if (!HTML) {
+    const HTML = catchPageHTML(name, pageUrl);
+    let $ = cheerio.load(HTML);
+    let pageListObj = $('#\\32 2b0be14d9504a8cbbf866f5e550a591 > div:nth-child(2) > div > a');
+    let pageList = [];
+    for (let i = 0; i < pageListObj.length; ++i) {
+        let pageObj = pageListObj[i];
 
-        return;
+        let page = {};
+        page.year = $(pageObj).text().trim();
+        page.url = DOMAIN + pageObj.href;
+        pageList.push(page);
     }
-    const $ = cheerio.load(HTML);
 
+    return pageList;
 }
 
 async function catchPageHTML(name, pageUrl) {
@@ -35,3 +58,5 @@ async function catchPageHTML(name, pageUrl) {
         return "";
     }
 }
+
+runMain();
--- learn/other/crawl.js
+++ learn/other/crawl.js
@@ -1,7 +1,20 @@
 const axios = require('axios');
 const cheerio = require('cheerio');
 
-axios.get('https://www.csdn.net/')
+axios.get('http://www.customs.gov.cn/customs/302249/zfxxgk/2799825/302274/302277/4899681/index.html',
+    {
+        headers: {
+            'Accept': 'application/json, text/javascript, */*; q=0.01',
+            'Accept-Encoding': 'gzip, deflate',
+            'Accept-Language': 'zh-CN,zh;q=0.9,en;q=0.8',
+            'Cache-Control': 'no-cache',
+            'Connection': 'keep-alive',
+            'Host': 'www.customs.gov.cn',
+            'Origin': 'http://www.customs.gov.cn',
+            'Cookie': '__jsluid_h=940adf28c45c5d5ba1579464cfed5333; AV7KYchI7HHaS=5bg1vCFS5WfLjd8rzBn6iIH1t6OXQSyWOGOkfFOQdRtxW6UDTjKqnYl_7Tkh52zwsCOfrdzsg.LOxKkUdxbtIVA; _gscu_1524496263=87326908zni1lx10; _gscbrs_1524496263=1; EPORTALJSESSIONID=3OQgZHv5ef1yYSPOgw055_ScSRR-oUKQG4spBJhkPiKaG964_nek!1029704743; __jsl_clearance=1688525751.598|0|%2FJIWzgWl%2FvlaKiCBNnloRxZ5xJQ%3D; AV7KYchI7HHaT=6FjGOyAh_y89mDoFYd63u6DjeXr.wtF30P47ubZTrrfjEaogbmOCZvl3ODIanabinAqRk0HrHdcibWMbE3mJXY501fdeMZOFpZNLPzLs_isgorRyR4qSDBHSyfrrTJedmevg9VE1G4gdklliaEr4KExYdJDVIzQxbnqFxPUzfPtMfVi1K.EJ5F_Roa.RwOBvU.SFwPCMNjnSKqyg2rAwegCoL9ici5BAeMQgQ9FXbLgZLf7TR5ktAJfiSgiZYjZtTFawC2t0xCaYMmXS8jyKhRsZ6OYYplGbIwxvRVuNWja1acuUcMhSyMSUXDLWj855M5.x4QKkiWEOPGKINZ4gBubF9lDf9IDTp4.VrQ6sHM3',
+            'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36'
+        }
+    })
     .then((response) => {
         const html = response.data;
         // console.log(html);
