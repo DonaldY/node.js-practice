2c953a10f8bc057b2d39453c92f05043db4d337e
yyf <448641125@qq.com>
2024-06-03 15:46:13 +0800

    feat: update

--- crawl/cpr/cpr_download.js
+++ crawl/cpr/cpr_download.js
@@ -92,16 +92,16 @@ async function downLoadFile(fileUrl, destinationFolder) {
         hostname: 'www.customs.gov.cn',
         path: fileUrl,
         method: 'GET',
-        'headers': {
-            'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
-            'Accept-Language': 'zh-CN,zh;q=0.9,en;q=0.8',
-            'Cache-Control': ' no-cache',
-            'Connection': ' keep-alive',
-            'Host': ' www.customs.gov.cn',
-            'Pragma': ' no-cache',
-            'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/116.0.0.0 Safari/537.36',
-            'Upgrade-Insecure-Requests': '1',
-            'Cookie': '__jsluid_h=456735be2d59c0d04e1f75e1b2ed67ac;__jsl_clearance=1717398527.897|0|9LRwSQdf4wZ49qWpUWmh%2Fw3AQjI%3D;'
+        "headers": {
+            "accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
+            "accept-language": "zh-CN,zh;q=0.9,en;q=0.8",
+            "cache-control": "no-cache",
+            "pragma": "no-cache",
+            "upgrade-insecure-requests": "1",
+            "cookie": "__jsluid_h=456735be2d59c0d04e1f75e1b2ed67ac;__jsl_clearance=1717398527.897|0|9LRwSQdf4wZ49qWpUWmh%2Fw3AQjI%3D;",
+            "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/125.0.0.0 Safari/537.36",
+            //"Referer": "http://www.customs.gov.cn/customs/302249/zfxxgk/2799825/302274/302277/302276/4245830/2022031814252873524.xls",
+            //"Referrer-Policy": "strict-origin-when-cross-origin"
         }
     };
 
